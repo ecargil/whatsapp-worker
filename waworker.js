a62c45d5bb186234934c900d51885e99a0724357
Carlos Gil <carlosgil83@gmail.com>
2026-02-04 11:47:58 +0100

    Initial commit: WhatsApp Worker

--- waworker.js
+++ waworker.js
@@ -3,19 +3,16 @@ export default {
     const url = new URL(request.url);
     let q = url.searchParams.get('q') || '';
 
-    // Limpiar todo excepto números y +
     let num = q.replace(/[^\d+]/g, '');
 
-    // Convertir 00XX → +XX
     if (num.startsWith('00')) {
       num = '+' + num.slice(2);
     }
 
-    // Quitar + para wa.me
     num = num.replace('+', '');
 
     if (!num) {
-      return new Response('Número no válido', { status: 400 });
+      return new Response('Number not valid', { status: 400 });
     }
 
     return Response.redirect('https://wa.me/' + num, 302);
